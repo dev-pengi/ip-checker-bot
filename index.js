5f5dea64f788e6c40333bc3dc1e2369623e65e6c
penguin-9999 <siffffdoul@gmail.com>
2022-08-23 04:14:30 +0100

    last test

--- index.js
+++ index.js
@@ -274,11 +274,6 @@ tracker.on('guildMemberAdd', (member, type, invite) => {
 
 
 
-
-
-fjhhhhhhhhhhhhhhhhhhhhhhhhhhg
-
-
 //------------------------------------------File Readder----------------------------------------
 
 
